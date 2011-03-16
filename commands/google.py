81ed4888bccb5a913e965a7181d61e1786d7f1b6
zeekay <none@none>
2011-03-15 21:48:10 -0500

    Rewrote youtube to merely wrap created command func

--- commands/google.py
+++ commands/google.py
@@ -6,11 +6,12 @@ GOOGLE_API_KEY = config['google_api_key']
 if GOOGLE_API_KEY:
     MY_IP = urllib.urlopen('http://www.whatismyip.com/automation/n09230945.asp').read()
     YOUTUBE_PATTERN = re.compile('(http://www.youtube.com[^\&]+)')
-    SEARCH = (
+    SEARCH_API = (
         {'url': 'web', 'aliases': ('g', 'google')},
         {'url': 'books', 'aliases': ('gb', 'books')},
         {'url': 'images', 'aliases': ('gi', 'images')},
         {'url': 'patents', 'aliases': ('gp', 'patents')},
+        {'url': 'video', 'aliases': ('yt', 'youtube')},
     )
 
     def build_search(api):
@@ -30,25 +31,17 @@ if GOOGLE_API_KEY:
         search.__doc__ = "searches %s" % api['url']
         return search
 
-    for api in SEARCH:
+    for api in SEARCH_API:
         setattr(sys.modules[__name__], api['url'], build_search(api))
 
-    @command('youtube', 'yt')
-    def google_video(self, nick, host, channel, *args):
-        """searches for videos"""
-        if args:
-            query = '%20'.join(args)
-        else:
-            return 'u need a query'
-        url = 'https://ajax.googleapis.com/ajax/services/search/video?v=1.0&q=%s&key=%s&userip=%s' % (query, GOOGLE_API_KEY, MY_IP)
-        request = urllib2.Request(url, None, {'Referer': 'http://quoth.notune.com'})
-        response = urllib2.urlopen(request)
-        results = json.load(response)['responseData']['results']
-        title, url = results[0]['titleNoFormatting'], results[0]['url']
+    @command('yt', 'youtube')
+    def youtube_search(*args):
+        """search youtube"""
+        msg, url = video(*args).rsplit(' ', 1)
         if 'youtube' in url:
             url = re.search(YOUTUBE_PATTERN, url).group()
             url = url.replace('%3F', '?').replace('%3D', '=')
-        return 'top result: %s - %s' % (title, url)
+        return ' '.join((msg, url))
 
     @command('gt', 'translate')
     def google_translate(self, nick, host, channel, language_pair=None, *args):
