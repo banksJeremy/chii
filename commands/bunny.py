d0297a1ab20ebe8701b31621b58fd404fad9f933
zeekay <none@none>
2011-03-23 11:50:30 -0500

    Rewrote bunny to take advantage of built-in asynch behavior

--- commands/bunny.py
+++ commands/bunny.py
@@ -6,4 +6,4 @@ PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'check_stream.py
 @command
 def bunny(self, channel, *args):
     """checks whetha bunny is streamin'"""
-    self.msg_defer_to_thread(channel, subprocess.check_output, ['python', PATH])
+    return subprocess.check_output(['python', PATH])
