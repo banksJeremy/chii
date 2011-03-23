8c44070391cec47f6bb9b861611b44a652dc89be
zeekay <none@none>
2011-03-23 17:50:15 -0500

    Made check_stream look in headers too

--- commands/check_stream.py
+++ commands/check_stream.py
@@ -7,10 +7,26 @@ HOST = 'freqbase.com'
 PORT = 8080
 
 class ShoutcastBunny(shoutcast.ShoutcastClient):
+    userAgent = "Shouty"
+
+    def __init__(self, path="/"):
+        self.path = path
+        self.got_metadata = False
+        self.metaint = None
+        self.metamode = "mp3"
+        self.databuffer = ""
+        self.headers = {}
+
+    def handleHeader(self, key, value):
+        if key.lower() == 'icy-metaint':
+            self.metaint = int(value)
+            self.got_metadata = True
+        self.headers[key] = value
+
     def gotMetaData(self, data):
         for key, value in data:
             if key == 'StreamTitle':
-                if 'bunny' in key:
+                if 'bunny' in key or 'bunny' in self.headers.values():
                     print 'bunny must be streamin! tune in @ http://freqbase.com/stream.m3u'
                 else:
                     print 'bunny pry not streaming :( tune in anywayz i guezz @ http://freqbase.com/stream.m3u'
