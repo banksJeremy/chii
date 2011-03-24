decc6a80eb610062fd07a96a6990e0895156f942
zeekay <none@none>
2011-03-24 10:05:33 -0500

    Cleaned up megachat a bit

--- events/megachat.py
+++ events/megachat.py
@@ -1,23 +1,34 @@
-import os, random, re
-from chii import event
+try:
+    from megahal import *
+except:
+    megahal = None
 
-from megahal import *
+if megahal:
+    import os, random, re
+    from chii import event
 
-BRAIN = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'megahal.brain')
-CHATTINESS = 0
-
-megahal = MegaHAL(brainfile=BRAIN, order=DEFAULT_ORDER, timeout=DEFAULT_TIMEOUT)
-
-@event('msg')
-def megachat(self, channel, nick, host, msg):
-    if self.nickname.lower() in msg.lower():
-        msg = re.compile(self.nickname + "[:,]* ?", re.I).sub('', msg)
-        prefix = "%s: " % nick
+    # get config or set defaults
+    if config['megahal_brain']:
+        BRAIN = config['megahal_brain']
     else:
-        prefix = ''
-
-    if prefix or random.random() <= CHATTINESS:
-        return prefix + megahal.get_reply(msg)
-
-    megahal.learn(msg)
-    megahal.sync()
+        BRAIN = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'megahal.brain')
+    if config['megahal_chattiness']:
+        CHATTINESS = config['megahal_chattiness']
+    else:
+        CHATTINESS = 0
+    
+    megahal = MegaHAL(brainfile=BRAIN, order=DEFAULT_ORDER, timeout=DEFAULT_TIMEOUT)
+    
+    @event('msg')
+    def megachat(self, channel, nick, host, msg):
+        if self.nickname.lower() in msg.lower():
+            msg = re.compile(self.nickname + "[:,]* ?", re.I).sub('', msg)
+            prefix = "%s: " % nick
+        else:
+            prefix = ''
+    
+        if prefix or random.random() <= CHATTINESS:
+            return prefix + megahal.get_reply(msg)
+    
+        megahal.learn(msg)
+        megahal.sync()
